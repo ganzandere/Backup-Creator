9c4e10e4402e6bc2e94d370dc1ba848352750108
Sibin <acimovic.sibin@gmail.com>
2023-03-31 01:51:48 +0200

    Added constants.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+WIN_W =700
+WIN_H = 650
+WIN_FONT = ("Inter", 12, "bold")
+
+FG_COLOR1 = "#e53935"
+FG_COLOR2 = "#4c1b1b"
+
+LOG_W = 660
+LOG_H = 400
--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from tkinter import filedialog as fd
 import customtkinter as ctk
 
 from helpers import get_drive_letters, file_copier
-
+import constants as c
 
 class App(ctk.CTk):
     """Creates a customtkinter GUI class."""
@@ -14,19 +14,18 @@ class App(ctk.CTk):
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
 
-        self.title("Backup Creator v0.1")
-        self.geometry("700x650")
-        self.maxsize(700, 650)
-        self.minsize(700, 650)
+        self.title("DK Backup Creator")
+        self.geometry(f"{c.WIN_W}x{c.WIN_H}")
+        self.maxsize(c.WIN_W, c.WIN_H)
+        self.minsize(c.WIN_W, c.WIN_H)
 
         self.grid_rowconfigure(5, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
         self.font = ctk.CTkFont("Inter",size=12,weight="bold")
-        self.icon = "dk.ico"
-        self.iconPath = self.resource_path(f'{os.path.dirname(__file__)}/icons/dk.ico')
-        self.iconbitmap(self.iconPath)
+        self.icon_path = f'{os.path.dirname(__file__)}/icons/dk.ico'
+        self.iconbitmap(self.icon_path)
 
         self.destination_frame = ctk.CTkFrame(master=self)
         self.destination_frame.grid(row=1, column=0, padx=10, pady=5, sticky="w")
@@ -34,7 +33,7 @@ class App(ctk.CTk):
         self.destination_letter_label = ctk.CTkLabel(master=self.destination_frame, text="Destination Drive",  font=self.font)
         self.destination_letter_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
-        self.destination_letter_optionbox = ctk.CTkOptionMenu(master=self.destination_frame, values=get_drive_letters(), width=60, font=self.font, fg_color="#e53935", button_color="#4c1b1b")
+        self.destination_letter_optionbox = ctk.CTkOptionMenu(master=self.destination_frame, values=get_drive_letters(), width=60, font=self.font, fg_color=c.FG_COLOR1, button_color=c.FG_COLOR2)
         self.destination_letter_optionbox.grid(row=0, column=1, padx=10, pady=10)
 
         self.files_frame = ctk.CTkFrame(master=self)
@@ -43,16 +42,16 @@ class App(ctk.CTk):
         self.files_entry = ctk.CTkEntry(master=self.files_frame, placeholder_text="Path to .txt file containing files for backup.", width=500,  font=self.font)
         self.files_entry.grid(row=0, column=0, padx=10, pady=10)
 
-        self.files_button = ctk.CTkButton(master=self.files_frame, text="Browse", font=self.font, command=self.browse_callback, fg_color="#4c1b1b")
+        self.files_button = ctk.CTkButton(master=self.files_frame, text="Browse", font=self.font, command=self.browse_callback, fg_color=c.FG_COLOR2)
         self.files_button.grid(row=0, column=1, padx=10, pady=10)
 
         self.backup_frame = ctk.CTkFrame(master=self)
         self.backup_frame.grid(row=1, column=0, padx=10, pady=5, sticky="e")
 
-        self.preview_button = ctk.CTkButton(master=self.backup_frame, text="Preview",  font=self.font, command=self.preview_callback, fg_color="#4c1b1b")
+        self.preview_button = ctk.CTkButton(master=self.backup_frame, text="Preview",  font=self.font, command=self.preview_callback, fg_color=c.FG_COLOR2)
         self.preview_button.grid(row=0, column=0, padx=10, pady=10)
 
-        self.backup_button = ctk.CTkButton(master=self.backup_frame, text="Backup",  font=self.font, command=self.backup_callback, fg_color="#e53935")
+        self.backup_button = ctk.CTkButton(master=self.backup_frame, text="Backup",  font=self.font, command=self.backup_callback, fg_color=c.FG_COLOR1)
         self.backup_button.grid(row=0, column=1, padx=10, pady=10)
 
         self.options = ctk.CTkFrame(master=self)
@@ -60,8 +59,6 @@ class App(ctk.CTk):
 
         self.options_entry = ctk.CTkEntry(master=self.options, width=500, font=self.font, placeholder_text="Additional path argument after the destination drive letter.")
         self.options_entry.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
-        #self.options_checkbox = ctk.CTkCheckBox(master=self.options, text='Insert Path', offvalue=False, onvalue=True)
-        #self.options_checkbox.grid(row=0, column=1, padx=10, pady=10, sticky='e')
         self.options_switch = ctk.CTkSwitch(master=self.options, text='Insert Path', offvalue=False, onvalue=True, switch_width=50)
         self.options_switch.grid(row=0, column=1, padx=10, pady=10, sticky='e')
 
@@ -70,7 +67,7 @@ class App(ctk.CTk):
 
         self.log_label = ctk.CTkLabel(master=self.log_frame, text='Log',  font=self.font)
         self.log_label.grid(row=0, column=0, padx=20, pady=5, sticky="w")
-        self.log_textbox = ctk.CTkTextbox(master=self.log_frame, width=660, height=400, font=self.font, wrap="none")
+        self.log_textbox = ctk.CTkTextbox(master=self.log_frame, width=c.LOG_W, height=c.LOG_H, font=self.font, wrap="none")
         self.log_textbox.grid(row=1, column=0, padx=10, pady=5, sticky='nsew')
 
     def browse_callback(self):
